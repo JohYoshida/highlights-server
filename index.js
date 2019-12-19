60eba902a3530cef56f2fcc280859cc2da0cdc8c
Joh Yoshida <johyoshida@gmail.com>
2019-12-19 15:14:52 -0800

    Basic server setup and routing

---
+++ index.js
@@ -0,0 +1,37 @@
+// Server requirements
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const moment = require("moment");
+const uuid = require("uuid/v1");
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+// Database requirements
+const dbconfig = require("./knexfile.js")[process.env.DB_ENV];
+const knex = require("knex")(dbconfig);
+
+// Router requirements
+const sessionsRouter = require("./routes/sessions")(knex);
+const strainsRouter = require("./routes/strains")(knex);
+const producersRouter = require("./routes/producers")(knex);
+const productsRouter = require("./routes/products")(knex);
+const purchasesRouter = require("./routes/purchases")(knex);
+
+// Parse application/json
+app.use(bodyParser.json());
+
+// Routers
+app.use("/sessions", sessionsRouter);
+app.use("/strains", strainsRouter);
+app.use("/producers", producersRouter);
+app.use("/products", productsRouter);
+app.use("/purchases", purchasesRouter);
+
+// Home
+app.get("/", (req, res) => {
+  res.send("Highlights server");
+});
+
+// Start app
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
