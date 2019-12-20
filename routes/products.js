021abb3a67896194ee6fd8e2452c84225d0baf22
Joh Yoshida <johyoshida@gmail.com>
2019-12-19 21:49:56 -0800

    Post and delete product by ID

--- routes/products.js
+++ routes/products.js
@@ -17,24 +17,42 @@ module.exports = () => {
       })
       .catch(err => {
         res.send({ err });
+        console.log(err);
       });
   });
 
-  // Post a new product
-  router.post("/", (req, res) => {
-    let product = {
-      id: uuid(),
-      producer_id: uuid(),
-      strain_id: uuid()
-    };
-    // Insert new product into db
+  // Post a new product by ID
+  router.post("/:producer_id/:strain_id", (req, res) => {
+    let id = uuid();
+    let { producer_id, strain_id } = req.params;
+    let product = { id, producer_id, strain_id }
+    // Check for existing product
     knex("products")
-      .insert(product)
-      .then(() => {
-        res.send({ msg: "Added to db", obj: product });
+      .first()
+      .where({ producer_id, strain_id })
+      .then(product => {
+        if (product) {
+          res.send({
+            msg: "A product with that information already exists."
+          });
+        } else {
+          // Insert new product into db
+          knex("products")
+            .insert(product)
+            .then(() => {
+              res.send({ msg: "Added to db", obj: product });
+            })
+            .catch(err => {
+              res.send({ err });
+              console.log(err);
+            });
+        }
       })
       .catch(err => {
-        res.send({ err });
+        res.send({
+          msg: "Failed database check. Error:\n" + err
+        });
+        console.log(err);
       });
   });
 
@@ -46,7 +64,23 @@ module.exports = () => {
         res.send({ msg: "Deleted all products" });
       })
       .catch(err => {
-        res.send({ err });
+        res.send({ msg: "Failed to delete all strains. Error:\n" + err });
+        console.log(err);
+      });
+  });
+
+  // Delete product by ID
+  router.delete("/:id", (req, res) => {
+    let { id } = req.params;
+    knex("products")
+      .where({ id })
+      .del()
+      .then(() => {
+        res.send({ msg: "Deleted product " + id });
+      })
+      .catch(err => {
+        res.send({ msg: "Failed to delete product. Error:\n" + err });
+        console.log(err);
       });
   });
 
